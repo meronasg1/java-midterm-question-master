4fcfdad0ad677b07055545fe0a60aa4c8f974499
= <=>
2021-05-03 22:47:18 -0400

    found pattern

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -1,18 +1,29 @@
-package math.problems;
-
-public class PrimeNumber {
-
-    public static void main(String[] args) {
-        /*
-         * Find list of Prime numbers from number 2 to 1 million.
-         * Try the best solution as possible.Which will take less CPU life cycle.
-         * Out put number of Prime numbers on the given range.
-         *
-         *
-         * Use MySql Database to store data and retrieve data.
-         *
-         */
-
-    }
-
-}
+//package math.problems;
+//
+//public class PrimeNumber {
+//
+//    public static void main(String[] args) {
+//        /*
+//         * Find list of Prime numbers from number 2 to 1 million.
+//         * Try the best solution as possible.Which will take less CPU life cycle.
+//         * Out put number of Prime numbers on the given range.
+//         *
+//         *
+//         * Use MySql Database to store data and retrieve data.
+//         *
+//         */
+//
+//    }
+//
+//    //method to check if a number is prime
+//    public static boolean checkForPrimeNumber(int maxNumber) {
+//        maxNumber = 1000000;
+//        for (int i = 2; i<maxNumber; i++){
+//           int  remainder=maxNumber/i;
+//            if (remainder==0){
+//                return false;
+//            }
+//            }
+//    }
+//    }
+//}
