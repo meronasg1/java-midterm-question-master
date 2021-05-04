a051cb97b9d166af7a38d43a0dd63b9f8400ee30
= <=>
2021-05-03 20:43:30 -0400

    found lowest number, still need to figure out how to use MySql Database to store and retrieve data

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -15,6 +15,14 @@ public class LowestNumber {
         int[] array = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
 
         //find lowest number from the array
+        int n = array.length;
+
+        int smallNumber=array[0];
+        for(int i=0; i<n; i++){
+            if (array[i]<=smallNumber){
+                smallNumber=array[i];}
+        }
+        System.out.println(smallNumber);
 
 
 
